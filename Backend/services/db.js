1212f4876cf073ccdef39e63f0e921fa1531be80
Julian.Oliva <102344556+JNO-22@users.noreply.github.com>
2025-02-21 15:54:01 -0300

    Add error handling to db

--- Backend/services/db.js
+++ Backend/services/db.js
@@ -6,11 +6,18 @@ const pool = mysql.createPool(config.db);
 async function query(sql, params) {
   const connection = await pool.getConnection();
   try {
-    const [results] = await connection.execute(sql, params);
+    const [results] = await connection.query(sql, params);
     return results;
+  } catch (error) {
+    if (error.code === "ER_ACCESS_DENIED_ERROR") {
+      throw new Error("Invalid credentials");
+    } else {
+      throw error;
+    }
   } finally {
     connection.release();
   }
+  // return pool.query(sql, params);
 }
 
 export default { query };
