dd2f382a79dba6469d16a77086bc8648d93358ba
JNO-22 <julian.oliva225@gmail.com>
2025-03-25 16:47:40 -0300

    fix: :bug: Añade manejo de errores al conectar con la BD y agrega configuracion de entorno usando env
    Agrega un return a db para evitar crasheos en el server e incluye un archivo .env para la configuracion de la conexion con la BD

--- Backend/services/db.js
+++ Backend/services/db.js
@@ -4,22 +4,29 @@ import config from "../config.js";
 const pool = mysql.createPool(config.db);
 
 async function query(sql, params) {
-  const connection = await pool.getConnection();
   try {
-    const [results] = await connection.query(sql, params);
+    const connection = await pool.getConnection();
+    try {
+      const [results] = await connection.query(sql, params);
 
-    if (results.length === 0) {
-      throw new Error("No se encontraron resultados");
+      if (results.length === 0) {
+        throw new Error("No se encontraron resultados");
+      }
+      return results;
+    } catch (error) {
+      if (error.errno === 1045) {
+        throw new Error("Invalid credentials");
+      } else {
+        throw error;
+      }
+    } finally {
+      connection.release();
     }
-    return results;
   } catch (error) {
-    if (error.errno === 1045) {
-      throw new Error("Invalid credentials");
-    } else {
-      throw error;
-    }
-  } finally {
-    connection.release();
+    const { code } = error;
+    console.log(error);
+    
+    return `Error al realizar la consulta: ${code}`;
   }
   // return pool.query(sql, params);
 }
