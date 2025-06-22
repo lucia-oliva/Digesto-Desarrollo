a5cc9bf1e9ea2516ca17eda2f250df5490fad720
Lucia Oliva <lucia222lr@gmail.com>
2025-06-22 20:48:19 -0300

    ksda

--- Digesto/src/pages/admin/Carga/config/formFields.js
+++ Digesto/src/pages/admin/Carga/config/formFields.js
@@ -3,6 +3,14 @@ export const camposPorEntidad = {
     { name: "numero", label: "Número", type: "text", required: true },
     { name: "anio", label: "Año", type: "text", required: true },
     { name: "titulo", label: "Título", type: "text", required: true },
+    {name: "resumen", label: "Resumen", type: "text", required: false},
+    {name: "fecha", label: "Fecha de publicación", type: "date", required: true},
+    {name:"dependencia", label:"Dependencia", type:"select", options:["Dependencia 1", "Dependencia 2"], required:true},
+    {name:"emisor", label:"Emisor", type:"select", options:["Emisor 1", "Emisor 2"], required:true},
+    {name:"estado", label:"Estado", type:"select", options:["Activo", "Inactivo"], required:true},
+    {name:"tags", label:"Tags", type:"text", required:false, onkeydown: },
+    {name:"archivo", label:"PDF", type:"file", required:true},
+    
     // etc...
   ],
   usuario: [
