31c0530b7093fc59cf463246af0fe19375da12a2
Lucia Oliva <lucia222lr@gmail.com>
2025-11-03 14:48:41 -0300

    modif .env

--- Backend/config/env.js
+++ Backend/config/env.js
@@ -2,7 +2,7 @@
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV ?? "development",
   PORT: Number(process.env.PORT ?? 3000),
-  FRONT_ORIGINS: (process.env.FRONT_ORIGINS ?? "http://172.20.3.147:5173")
+  FRONT_ORIGINS: (process.env.FRONT_ORIGINS ?? "http://localhost:5173")
     .split(",")
     .map((s) => s.trim()),
 };
